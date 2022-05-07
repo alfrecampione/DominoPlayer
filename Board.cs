c146dbbc86fa5e7620d98a41bd954c836b63ab0a
alfrecampione <80720402+alfrecampione@users.noreply.github.com>
2022-05-07 03:55:30 -0400

    Developing player

---
+++ Board.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominoPlayer
+{
+    public class Board
+    {
+        int actualTurn;
+        public int ActualTurn
+        {
+            get { return actualTurn; }
+        }
+    }
+}
