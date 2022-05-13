1ffa11792e4e1fe031fb829569c342f141af99df
alfrecampione <80720402+alfrecampione@users.noreply.github.com>
2022-05-12 21:54:51 -0400

    Board created

--- Board.cs
+++ Board.cs
@@ -13,5 +13,41 @@ namespace DominoPlayer
         {
             get { return actualTurn; }
         }
+        List<Piece> board;
+        List<Piece> undistributedPieces;
+        
+        int piecesPerHand;
+        public Board(int numberOfPlayers, int piecesPerHand, int maxValue)
+        {
+            this.undistributedPieces = new List<Piece>();
+            this.board = new List<Piece>();
+            this.actualTurn = 1;
+            this.piecesPerHand = piecesPerHand;
+            CreateAllPieces(maxValue);
+        }
+
+        void CreateAllPieces(int maxValue)
+        {
+            for (int i = 0; i <= maxValue; i++)
+            {
+                for (int j = i; j <= maxValue; j++)
+                {
+                    undistributedPieces.Add(new(i, j));
+                }
+            }
+        }
+            
+        List<Piece> CreateHand()
+        {
+            Random random = new Random();
+            List<Piece> hand = new();
+            for (int i = 0; i < piecesPerHand; i++)
+            {
+                int index = random.Next(undistributedPieces.Count);
+                hand.Add(undistributedPieces[index]);
+                undistributedPieces.RemoveAt(index);
+            }
+            return hand;
+        }
     }
 }
