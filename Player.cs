f738bc950d364039f1f38e81305dc4be01eb9402
alfrecampione <80720402+alfrecampione@users.noreply.github.com>
2022-05-15 13:32:14 -0400

    Tester add

--- Player.cs
+++ Player.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DominoPlayer
 {
-    class Player
+    public class Player
     {
         List<Piece> hand;
         Board board;
--- Test.cs
+++
@@ -1,5 +0,0 @@
-using DominoPlayer;
-
-Board board = new(2, 10, 6, 2);
-Player p1 = new(board.CreateHand(), board, 1, false);
-Player p2 = new(board.CreateHand(), board, 2, false);
