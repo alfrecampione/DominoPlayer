bc5e5d3a166334d322dc3799bd2a033e5da28da1
PixelDG <35879142+pixeldashgames@users.noreply.github.com>
2022-06-06 00:32:56 -0400

    Made Move ctor inline.

--- DominoPlayer/Move.cs
+++ DominoPlayer/Move.cs
@@ -8,12 +8,8 @@ public struct Move
     public bool placedOnRight;
 
     public Move(int playerID, Piece piece, bool passed, bool placedOnRight)
-    {
-        this.playerID = playerID;
-        this.piecePlaced = piece;
-        this.passed = passed;
-        this.placedOnRight = placedOnRight;
-    }
+    => (this.playerID, this.piecePlaced, this.passed, this.placedOnRight)
+        = (playerID, piece, passed, placedOnRight);
 
     public static Move CreateMove(int playerID, Piece piece, bool right) => new(playerID, piece, false, right);
     public static Move CreatePass(int playerID) => new(playerID, default, true, default);
