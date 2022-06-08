54a824820f119c92f592fb4e41c373a53a9186df
leoamaro01 <leoamaro2001@gmail.com>
2022-06-07 22:24:18 -0400

    Modified behaviour of GetMove
    Now it isn't an abstract method, it is a decorator-like method
    It calls InternalGetMove (which is the abstract method now)
    After that it removes the piece used in the move from the player's hand
    
    Added the GetPlayerScore method, which uses rules to determine a player's score

--- DominoPlayer/DominoPlayer.cs
+++ DominoPlayer/DominoPlayer.cs
@@ -1,16 +1,27 @@
+using System.Runtime.Serialization;
 using System.Collections.Generic;
+using System.Linq;
 namespace DominoPlayer
 {
     public abstract class DominoPlayer
     {
         public int PlayerID { get; }
         protected DominoGame GameReference { get; }
-        public List<Piece> Hand { get; set; }
+
+        protected List<Piece> Hand { get; }
         public int Count { get { return Hand.Count; } }
         public DominoPlayer(int playerID, DominoGame game)
         => (PlayerID, GameReference, Hand) = (playerID, game, new List<Piece>());
-        public abstract Move GetMove();
-        public virtual int PiecesInHand() => Hand.Count;
+        public double GetPlayerScore() => GameReference.gameRules.GetHandScore(GameReference, Hand);
+        public Move GetMove()
+        {
+            Move move = InternalGetMove();
+            if (!move.passed)
+                Hand.RemoveAt(Hand.FindIndex(p => p == move.piecePlaced));
+            return move;
+        }
+        protected abstract Move InternalGetMove();
+        public virtual int PiecesInHand => Hand.Count;
         public virtual void StartGame(IEnumerable<Piece> startingHand)
         {
             Hand.Clear();
